664bb4aba21c6f6bd0a44863f3b6ee58b7623c94
Lucas Miranda <lucasgmm@usp.br>
2022-04-19 11:29:46 -0300

    Created repository interfaces

---
+++ src/main/java/com/lucas/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.lucas.bookstore.repositories;
+
+import com.lucas.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
---
+++ src/main/java/com/lucas/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.lucas.bookstore.repositories;
+
+import com.lucas.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
