f33daa3b9d974b2b9f72baa77341786007dd28fc
Lucas Miranda <lucasgmm@usp.br>
2022-04-20 10:19:15 -0300

    Added findAll method for the 'categoria' resource (using the DTO pattern)

--- src/main/java/com/lucas/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/lucas/bookstore/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.lucas.bookstore.resources;
 
 import com.lucas.bookstore.domain.Categoria;
+import com.lucas.bookstore.dtos.CategoriaDTO;
 import com.lucas.bookstore.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -20,5 +24,12 @@ public class CategoriaResource {
     public ResponseEntity<Categoria> findById(@PathVariable Integer id){
         Categoria obj = service.findById(id);
         return ResponseEntity.ok(obj);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok(listDTO);
     }
 }
